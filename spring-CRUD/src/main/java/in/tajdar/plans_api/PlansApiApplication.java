2a0b9ed38e21d2b662cb3c812da90f708100e0b0
mrAhmedAbdullah <ahmeabdullah786@gmail.com>
2026-09-22 19:35:22 +0530

    trying with termux

--- spring-CRUD/src/main/java/in/tajdar/plans_api/PlansApiApplication.java
+++ spring-CRUD/src/main/java/in/tajdar/plans_api/PlansApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PlansApiApplication {
-
+//Currently using termux from mobile, I am trying to use github from mobile (termux)
 	public static void main(String[] args) {
 		SpringApplication.run(PlansApiApplication.class, args);
 	}
